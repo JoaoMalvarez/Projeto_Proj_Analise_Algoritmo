b830c647b9afb71aff410f575a98f44c739a336c
ychbr <yamadacah@gmail.com>
2026-04-05 17:58:14 -0300

    Update identifica.c

--- Idiomas/identifica.c
+++ Idiomas/identifica.c
@@ -107,11 +107,14 @@ int main () {
 
     tamanho++; // Adiciona 1 para contar a última palavra
 
+    // Calcula a taxa de erro
+    // Diminui de acordo com a quantidade de letras
+ 
     float taxaErro;
  
-    if (tamanho > 0 && tamanho <= 10) taxaErro = 0.15;
+    if (tamanho > 0 && tamanho <= 10) taxaErro = 0.15; // Menos letras = maior taxa
     if (tamanho > 10 && tamanho <= 40) taxaErro = 0.1;
-    if (tamanho > 40) taxaErro = 0.05;
+    if (tamanho > 40) taxaErro = 0.05; // Mais letras = menor taxa
  
     verificaQnt(frase, freq); // Chamada da função para contar a quantidade de vezes que cada letra aparece na frase
  
