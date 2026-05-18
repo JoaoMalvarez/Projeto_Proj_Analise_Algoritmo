e96419c71912e420b72fdfc8fd3630a5ea0268df
Catarina Silva e Meirelles <catlls622@gmail.com>
2026-05-18 11:00:08 +0000

    criando xeque estrutura

---
+++ Xeque/xeque.c
@@ -0,0 +1,21 @@
+/*
+Nome: Catarina Silva e Meirelles, RA:10239324
+Nome: João Pedro Mazzante Alvarez, RA:10723837
+Nome: Lívia Calado de Carvalho Dias, RA: 10737709
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h> // para strlen
+
+void vetorizarForsyht(){}
+
+void preve....
+.....
+
+void verificaXeque(){}//chama todas as preves
+
+int main(){
+    //recebe a string e passa para a verifica que chama o forsyth e as preves, ela printa o resultado tbm
+    return 0;
+}
