5597970b8cf72c5c3ac844318905732f90e88e21
liv-dias <10737709@mackenzista.com.br>
2026-05-18 12:31:10 +0000

    parte forsyth

--- Xeque/xeque.c
+++ Xeque/xeque.c
@@ -8,7 +8,42 @@ Nome: Lívia Calado de Carvalho Dias, RA: 10737709
 #include <ctype.h>
 #include <string.h> // para strlen
 
-void vetorizarForsyht(){}
+void vetorizarForsyth(char forsyth[]){
+    const char quebra[] = "-";
+    char destino[8][8];
+    char *parte = strtok(forsyth, quebra);
+    int i = 0;
+    
+    while(parte != NULL && i < 8){
+        int j = 0; // Controla a coluna atual do tabuleiro
+        
+        for (int k = 0; parte[k] != '\0' && j < 8; k++){
+            if(isdigit(parte[k])){
+                int casas_vazias = parte[k] - '0';
+                
+                for(int v = 0; v < casas_vazias && j < 8; v++){
+                    destino[i][j] = '_';
+                    j++; 
+                }
+            }
+            else{
+                destino[i][j] = parte[k];
+                j++;
+            }
+        }
+        
+        i++;
+        parte = strtok(NULL, quebra);
+    }
+    
+    for (int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            printf("%c ,", destino[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 
 void preveDama(){}
 void preveCavalo(){}
